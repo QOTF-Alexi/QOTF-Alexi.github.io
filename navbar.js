2a664083d0a6fb30e676e4f5ca2087828eac3052
Alexi <an.prins@protonmail.com>
2025-12-17 19:04:28 +0100

    Add a 2025 recap.

--- navbar.js
+++ navbar.js
@@ -3,7 +3,8 @@
     <nav>
       <button onclick="location.href = '../index.html';">Main Page</button>
       <button onclick="location.href = '../trans.html';">The Transitioning</button>
-      <button onclick="location.href = '../docs.html';">Random Things</button>
+      <button onclick="location.href = '../tech.html';">Tech Things</button>
+      <button onclick="location.href = '../randomthoughts.html';">Random Stuff</button>
     </nav>
   `;
     document.getElementById("navbar").innerHTML = navbarHTML;
