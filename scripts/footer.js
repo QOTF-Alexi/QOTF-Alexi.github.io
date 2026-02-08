60627bfa567572b849f0f1bc5d77245b0813f1d3
Alexi <an.prins@protonmail.com>
2026-02-08 16:13:20 +0100

    Move stuff around for compliance, extract footer to script.

---
+++ scripts/footer.js
@@ -0,0 +1,10 @@
+﻿function loadFooter() {
+    const footerHTML = `
+    <footer>
+    <hr>
+    <p style="font-size:75%;color:white;">(c)2026 Alexi, hosted on github.io | Photo by me! | Colours derived from <a
+            href="https://www.ppgpaints.com/">PPG Paint colours</a>.</p>
+</footer>
+  `;
+    document.getElementById("footer").innerHTML = footerHTML;
+}
